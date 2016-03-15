6b5ed325945258f527f4d34b1a3e8c55f2011ecb
dbillod <david.billod.de.filiquier@ensae.fr>
2016-03-15 12:20:50 +0100

    ajout do parallel

--- Code1_Big_data_David.R
+++ Code1_Big_data_David.R
@@ -21,6 +21,7 @@ base_a = read.csv("C:/Users/David/Documents/ENSAE/3A/Big Data et assurance/BNP C
 head(base_a)
 base_a[1,1]
 dim(base_a)
+
 attach(base_a)
 
 
@@ -79,6 +80,7 @@ for (i in 1:131){
 
 }
 VariableCat
+sum(VariableCat)
 posVarCat=which(VariableCat==1)
 posVarCat
 
@@ -125,9 +127,45 @@ test_indep_quali
 
 
 rf1 = randomForest(as.factor(target)~.,data  = base_a
-, ntree = 10
+, na.action = na.omit
+, ntree = 5
 , mtry = 5)
 rf1
+
+rf2 = rfImpute(x = base_a, y = as.factor(target), ntree = 10)
+
+
+library(parallel)
+library(foreach)
+install.package('doParallel')
+library(doParallel)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+
+registerDoParallel(cl)
+rf <- foreach(ntree=rep(100, no_cores), .combine= c, .packages='randomForest') %dopar% {
+  #randomForest((Surv1==1)~Gender+Type+Category
+  #             +Occupation +Age +Group1 +Bonus
+  #             +Poldur +Value +Adind +Group2 +Density,
+  #             data=base_test,
+  #             ntree=ntree, importance=TRUE, keep.forest=TRUE)
+
+
+
+}
+stopCluster(cl)
+
+library(randomForest)
+varImpPlot (rf , main ="")
+importance (rf)
+
+courbeROC(rf,base_pred$Surv1,"Random Forest")?
+
+
 
 
 
