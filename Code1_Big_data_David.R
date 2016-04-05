122e0f3712361fd8ba2c7e27c608fcef50a4954a
dbillod <david.billod.de.filiquier@ensae.fr>
2016-04-05 11:31:49 +0200

    Petite randomforest et premières ACM

--- Code1_Big_data_David.R
+++ Code1_Big_data_David.R
@@ -79,6 +79,7 @@ for (i in 1:131){
 	}
 
 }
+
 VariableCat
 sum(VariableCat)
 posVarCat=which(VariableCat==1)
@@ -138,6 +139,11 @@ test_indep_quali
 #///////////////////////////////#
 
 
+##''''''''''''''''''''''''''''''''##
+## Premier essai de Random Forest ##
+##''''''''''''''''''''''''''''''''##
+
+
 rf1 = randomForest(as.factor(target)~.,data  = base_a
 , na.action = na.omit
 , ntree = 5
@@ -146,7 +152,17 @@ rf1
 
 rf2 = rfImpute(x = base_a, y = as.factor(target), ntree = 10)
 
-##On va enlever les variables avec plus de 53 modalités
+##''''''''''''''''''''''''''''''''##
+## CCL : trop grosse base 	    ##
+##''''''''''''''''''''''''''''''''##
+
+
+
+
+
+
+
+##On va enlever les variables avec plus de 53 modalités, car randomForest ne veut pas les gérer
 posvar_53 = NULL
 for (i in 1:length(posVarCat)){
 	if (nb_levels[2,i]>=53){
@@ -155,7 +171,11 @@ for (i in 1:length(posVarCat)){
 }
 posvar_53
 
+#####
+## Il y a 3 variables à plus de 53 modalités
+#####
 base_a1 = base_a[,-(posvar_53+2)]
+
 rf3 = rfImpute(x = base_a1[,-c(1,2)], y = as.factor(base_a1$target), ntree = 10)
 
 memory.size()
@@ -164,9 +184,97 @@ memory.limit(size = 98000)
 listindiv = sample(dim(base_a)[1], 100, T)
 rf4 = rfImpute(x = base_a1[listindiv,-c(1,2)], y = as.factor(base_a1$target)[listindiv], ntree = 10)
 
+
 varcible = as.factor(base_a1$target)[listindiv]
-randomforest1 = randomForest(varcible ~base_a1[listindiv,-c(1,2)],  rf4)
-importance(randomForest1)
+
+#randomforest1 = randomForest(varcible ~rf4[,2:129],  rf4)
+randomforest1 = randomForest(y = varcible, x = rf4[,2:129],  data=rf4)
+importance(randomforest1)
+varImpPlot(randomforest1)
+
+
+## -> On a réussi à faire une random forest sur une sous-base : 
+##		seulement 100 lignes
+##		on a retiré les variables catégorielles avec trop de modalités
+
+
+
+## -> On va bootstraper : on va tirer M fois un échantillon de N personnes
+## Ensuite, on va regarder quelles sont les variables qui ont la plus haute importance, en leur donnant un score
+## Si une variable arrive "en tête" de l'importance, alors on lui donne un score de 10
+##														5 pour la 2eme
+##														1 pour la 3ème
+
+M = 100
+N = 1000
+resimportance = rep(0, dim(base_a)[2]-2)
+
+## On enlève 2 variables : ID, target
+## Pour 3 autres : celles de plus de 53 modalités, on met -1
+resimportance[posvar_53] = -1
+
+for (i in 1:M){
+	listindiv = sample(dim(base_a)[1], N, T)
+	rf4 = rfImpute(x = base_a1[listindiv,-c(1,2)], y = as.factor(base_a1$target)[listindiv], ntree = 10)
+
+
+	varcible = as.factor(base_a1$target)[listindiv]
+
+	randomforest1_boot = randomForest(y = varcible, x = rf4[,2:129],  data=rf4)
+	
+	max1 = which(importance(randomforest1_boot) == max(importance(randomforest1_boot)))
+	max2 = which(importance(randomforest1_boot)[-max1] == max(importance(randomforest1_boot)[-max1]))
+	max3 = which(importance(randomforest1_boot)[-c(max1,max2)] == max(importance(randomforest1_boot)[-c(max1,max2)]))
+	
+	#Il faut voir s'il faut augmenter max1,...,max3 car on a enlevé certaines variables (>53 modalités)
+	#Donc les indices ne correspondent plus forcément aux variables
+	
+	max1 = max1 + sum(posvar_53-max1<=0)
+	max2 = max2 + sum(posvar_53-max2<=0)
+	max3 = max3 + sum(posvar_53-max3<=0)
+	
+	
+	resimportance[max1] = resimportance[max1] + 10
+	resimportance[max2] = resimportance[max2] + 5
+	resimportance[max3] = resimportance[max3] + 1
+
+}
+
+resimportance
+which(resimportance>0)
+
+## -> On va désormais essayer d'intégrer l'ensemble des lignes
+
+
+
+rf3_1 = rfImpute(x = base_a1[,-c(1,2)], y = as.factor(base_a1$target), ntree = 10)
+
+
+#==================================#
+# 	Tentatvie ACM		     #
+#==================================#
+
+library(FactoMineR)
+
+
+listeMCA = sample(dim(base_a)[1], 1000, T)
+
+dataMCA1 = base_a1[listeMCA, 2+posVarCat]
+MCA1 = MCA(dataMCA1
+MCA1
+MCA1$var$contrib
+
+dataMCA2 = base_a1[listeMCA, 2+which(resimportance>0)]
+MCA2 = MCA(dataMCA2)
+MCA2
+
+##On obtient 2-3 groupes de variables
+
+
+
+
+
+
 
 
 library(parallel)
@@ -180,23 +288,21 @@ no_cores <- detectCores() - 1
 cl <- makeCluster(no_cores)
 
 registerDoParallel(cl)
-rf <- foreach(ntree=rep(100, no_cores), .combine= c, .packages='randomForest') %dopar% {
-  #randomForest((Surv1==1)~Gender+Type+Category
-  #             +Occupation +Age +Group1 +Bonus
-  #             +Poldur +Value +Adind +Group2 +Density,
-  #             data=base_test,
-  #             ntree=ntree, importance=TRUE, keep.forest=TRUE)
+rf1 <- foreach(ntree=rep(100, no_cores), .combine= c, .packages='randomForest') %dopar% {
+	rf4_cluster = rfImpute(x = base_a1[,-c(1,2)], y = as.factor(base_a1$target), ntree = 10)
 
-
+  randomForest(y = rf4_cluster[,1], x = rf4_cluster[,-1],
+               data=rf4_cluster,
+               ntree=ntree, importance=TRUE, keep.forest=TRUE)
 
 }
 stopCluster(cl)
 
 library(randomForest)
-varImpPlot (rf , main ="")
-importance (rf)
+varImpPlot (rf1 , main ="")
+importance (rf1)
 
-courbeROC(rf,base_pred$Surv1,"Random Forest")?
+courbeROC(rf1,base_pred$Surv1,"Random Forest")?
 
 
 
